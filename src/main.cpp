c32f10b41fd5cf6e0171ec181b73b0e58029584c
Bas Bieling <bas@scrumble.nl>
2025-04-11 21:58:40 +0200

    wip, not what I wanted in the end

--- src/main.cpp
+++ src/main.cpp
@@ -74,40 +74,128 @@ IC   IC    Pin
 
 #include <Arduino.h>
 #include "F5KPGEUIK2UZ059.h"
+#include "optional.h"
 
 #define MAX_PINS 16
+#define DELAY_CLOCK 10
+#define DELAY_STAGE 2
 
-int Pin14[14] = {7, 8, 9, 10, 11, 12, A7, 5, 4, 3, 2, 13, A0, A1};        // contains the correct allocation for a 14 Pins IC
-int Pin16[16] = {6, 7, 8, 9, 10, 11, 12, A7, 5, 4, 3, 2, 13, A0, A1, A2}; // contains the correct allocation for a 16 Pins IC
 int PinMap[16] = {6, 7, 8, 9, 10, 11, 12, A7, 5, 4, 3, 2, 13, A0, A1, A2}; // contains the correct allocation for a 16 Pins IC
 
 char dat_read(unsigned int eeaddress);
+String dat_read_line(unsigned int addr);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600);  
+  
+  while (!Serial.available());
 }
 
-typedef struct TestSuite {
-  char name[8];
+struct TestSuite {
+  String name;
   char pinCount;
   unsigned int offset;
 };
 
-typedef struct TestResult { 
+struct TestResult { 
   bool pass;
-  bool state[MAX_PINS];
+  char state[MAX_PINS];
 };
+
+Optional<TestSuite> next(unsigned int offset) 
+{
+  for (char c = dat_read(offset); c != '$'; offset++, c = dat_read(offset)) {
+    if (c == 0) {
+      return {};
+    }
+  }
+
+  offset++;
+
+  String name = dat_read_line(offset);
+  offset += name.length() + 1;
+
+  String pinCount = dat_read_line(offset);
+  offset += pinCount.length() + 1;
+
+  name.trim();
+
+  return TestSuite{
+      name,
+      (char) pinCount.toInt(),
+      offset,
+  };
+}
 
 int* getPinDef(char pins)
 {
   return PinMap + (8 - (pins / 2));
 }
 
-TestResult test(char pins, char* definition)
+TestResult test(char pins, const char* definition)
 {
-  // Stage 1 set input signals
-  
+  int* pinDef = getPinDef(pins);
+
+  // Stage 1 setup pins and signals
+  for(char pin = 0; pin < pins; pin++) {
+    int p = pinDef[pin];
+    char def = definition[pin];
+
+    switch(def) {
+      case 'L':
+      case 'H':
+        pinMode(p, INPUT_PULLUP);
+        break;
+      
+      default:
+        pinMode(p, OUTPUT);
+        digitalWrite(p, def == 'V' || def == '1');
+        break;
+    }
+  }
+
+  delay(DELAY_STAGE);
+
+  // Stage 2 clock
+  for(char pin = 0; pin < pins; pin++) {
+    if(definition[pin] == 'C') {
+        digitalWrite(pinDef[pin], HIGH);
+    }
+  }
+
+  delay(DELAY_CLOCK);
+
+  for(char pin = 0; pin < pins; pin++) {
+    if(definition[pin] == 'C') {
+        digitalWrite(pinDef[pin], LOW);
+    }
+  }
+
+  delay(DELAY_STAGE);
+
+  // Stage 3 read back
+  TestResult result{true};
+  memset(result.state, 0, MAX_PINS);
+
+  for (int pin = 0; pin < pins; pin++) {
+    int p = pinDef[pin];
+    char def = definition[pin];
+
+    switch(def) {
+      case 'L':
+      case 'H':
+        result.state[pin] = digitalRead(p) ? 'H' : 'L';
+        result.pass &= result.state[pin] == def;
+        break;
+
+      default:
+        result.state[pin] = def;
+        break;
+    }
+  }
+
+  return result;
 }
 
 char dat_read(unsigned int eeaddress)
@@ -118,4 +206,160 @@ char dat_read(unsigned int eeaddress)
   }
 
   return pgm_read_byte(&F5KPGEUIK2UZ059_dat[eeaddress]);
+}
+
+String dat_read_line(unsigned int addr) 
+{
+  unsigned short count;
+  String output;
+  char c;
+
+  while((c = dat_read(addr + count)) != '\n') {
+    output += c;
+
+    count++;
+  }
+
+  return output;
+}
+
+void listChips() {
+  int count = 1;
+
+  for(Optional<TestSuite> suite = next(0); suite.has_value(); suite = next(suite.take().offset + 1)) {
+    Serial.print(" - ");
+    Serial.println(suite.take().name);    
+    count++;
+  }
+
+  Serial.println();
+  Serial.print("Found ");
+  Serial.print(count, DEC);
+  Serial.print(" test suites");
+  Serial.println();
+}
+
+Optional<TestSuite> find(String name) 
+{
+  for(Optional<TestSuite> suite = next(0); suite.has_value(); suite = next(suite.take().offset + 1)) {
+    if (suite.take().name == name) {
+      return suite;
+    }
+  }
+
+  return {};
+}
+
+String readline() {
+  // shit todo rewrite
+  char c;
+  String line = "";
+
+  while(Serial.available() == 0);
+
+  while ((c = Serial.read()) != '\n') {
+    if (c == '\r') {
+      if (line.length() > 0) {
+        line.remove(line.length() - 1);
+        Serial.print("\r \r");
+      }
+    } else {
+      Serial.print(c); 
+      line += c;
+    }    
+
+    while(Serial.available() == 0);
+  }
+
+  Serial.println();
+  line.trim();
+
+  return line;
+}
+
+void manualMode() {
+  Serial.print("Pin count: ");
+  while (Serial.available() == 0);
+  int pinCount = Serial.parseInt();
+
+  Serial.println("Manual mode, blank line to abort");
+
+  while (true) {
+    Serial.print("> ");
+    while (Serial.available() == 0);
+    String line = Serial.readString();
+    line.trim();
+
+    if (line.length() == 0) {
+      return;
+    }
+
+    if (line.length() != pinCount) {
+      Serial.print("ERR, expected definition of size ");
+      Serial.print(pinCount, DEC);
+      Serial.print(" got ");
+      Serial.print(line.length(), DEC);
+      Serial.println();
+      continue;
+    }
+
+    TestResult result = test(pinCount, line.c_str());
+
+    Serial.print("> ");
+    Serial.print(result.state);
+    Serial.println(result.pass ? " √" : " X");
+  }
+}
+
+void runSuite(TestSuite suite) {
+  Serial.print("Running suite: ");
+  Serial.println(suite.name);
+
+  
+} 
+
+void testMode() {
+  Serial.print("Chip: ");
+  String name = readline();
+
+  Optional<TestSuite> suite = find(name);
+  if (suite.has_value()) {
+    runSuite(suite.take()); 
+  } else {
+    Serial.print("ERR not found: '");
+    Serial.print(name);
+    Serial.println("'");
+  }
+}
+
+void loop() 
+{
+  Serial.println("Chip tester");
+  Serial.println("[m] Manual mode");
+  Serial.println("[t] Test chip");
+  Serial.println("[l] List definitions");
+  Serial.print("> ");
+  while (Serial.available() == 0);
+  Serial.println();
+
+  switch(Serial.read()) {
+    case 'm':
+    case 'M':
+      manualMode();
+      break;
+      
+    case 't':
+    case 'T':
+      testMode();
+      break;
+
+    case 'l':
+    case 'L':
+      listChips();
+      break;
+
+    default:
+      Serial.println("Uknown command");
+      break;
+  }
 }
